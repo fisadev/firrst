8cb565f443f85db71a918f092e2b8a73bca8d686
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-03-14 23:58:30 -0300

    Some basic styles and placeholders

--- firrst/app/views.py
+++ firrst/app/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 
 def home(request):
-    return render_to_response('home.html', {})
+    return render(request, 'home.html', {})
