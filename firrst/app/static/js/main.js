4342bfdf7699a7c62e162eaecb26055dbb8668e6
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-03-15 19:54:01 -0300

    Sidebar jquerized when exists. And will use a gui namespace

--- firrst/app/static/js/main.js
+++ firrst/app/static/js/main.js
@@ -2,15 +2,17 @@ var urls = {};
 urls.ajax_prefix = '/ajax/';
 urls.get_sidebar = urls.ajax_prefix + 'get_sidebar';
 
-var sidebar = $('#sidebar');
+var gui = {};
 
 function get_sidebar() {
     request = $.ajax({url: urls.get_sidebar});
     request.done(function(msg) {
-        sidebar.html(msg);
+        gui.sidebar.html(msg);
     });
 }
 
 $(document).ready(function() {
+    gui.sidebar = $('#sidebar');
+
     get_sidebar();
 });
