a6e8425116ed0fbdab32b80695ed14779aa3ad3e
Juan Pedro Fisanotti <fisadev@gmail.com>
2013-03-14 23:35:34 -0300

    Basic project and two models

---
+++ firrst/app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Feed(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.TextField()
+    last_fetch = models.DateTimeField()
+
+
+class Post(models.Model):
+    feed = models.ForeignKey(Feed)
+    fetched_at = models.DateTimeField()
+    read = models.BooleanField(default=False)
+    title = models.TextField()
+    content = models.TextField()
